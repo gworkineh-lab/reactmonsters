c98caea4268451f2b58f0d5ec008049f52bb85a2
Workineh <g.workineh@hotmail.com>
2019-08-20 00:03:54 -0400

    Updates

---
+++ precache-manifest.f72f787773523be4f50b9dcaca5cfdc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637e77dd75f177d5f9d4606e61a1e0f9",
+    "url": "/reactmonsters/index.html"
+  },
+  {
+    "revision": "40dd7ef73e9e4b01be1c",
+    "url": "/reactmonsters/static/css/main.48c92fc7.chunk.css"
+  },
+  {
+    "revision": "0b9069a31ad7a1a15d2f",
+    "url": "/reactmonsters/static/js/2.af006518.chunk.js"
+  },
+  {
+    "revision": "40dd7ef73e9e4b01be1c",
+    "url": "/reactmonsters/static/js/main.9f4a6c50.chunk.js"
+  },
+  {
+    "revision": "e6184bf1289a5c93a3e8",
+    "url": "/reactmonsters/static/js/runtime~main.29451554.js"
+  }
+]);
